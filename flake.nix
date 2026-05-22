3756f79396c6af67336b2db76b33118d7ed5dd86
fyrrmi <198573042+fyrrmi@users.noreply.github.com>
2026-05-22 09:29:13 +0200

    build(flake): switch nixpkgs to unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs, ... }: {
