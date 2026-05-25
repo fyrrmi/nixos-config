bcc0d850d643c26eb8f3be0de2cb6568403fdb9d
fyrrmi <198573042+fyrrmi@users.noreply.github.com>
2026-05-25 15:59:55 +0200

    feat: wayle autostart

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -48,11 +48,11 @@ local menu        = "rofi -show drun"
 -- Autostart necessary processes (like notifications daemons, status bars, etc.)
 -- Or execute your favorite apps at launch like this:
 --
--- hl.on("hyprland.start", function ()
---   hl.exec_cmd(terminal)
+hl.on("hyprland.start", function ()
+    hl.exec_cmd("wayle panel start")
 --   hl.exec_cmd("nm-applet")
 --   hl.exec_cmd("waybar & hyprpaper & firefox")
--- end)
+end)
 
 
 -- environment variables set here are passed to all processes launched by hyprland.
