0430617cfd0253b3d8bfc90619ac046d791778ab
anoscetia <198573042+anoscetia@users.noreply.github.com>
2026-06-15 10:04:44 +0200

    feat(sommei): set nix fish as login shell

--- hosts/sommei/default.nix
+++ hosts/sommei/default.nix
@@ -1,10 +1,16 @@
 { pkgs, ... }:
 {
-  nixpkgs.hostPlatform = "aarch64-darwin";
-  nixpkgs.config.allowUnfree = true;
-  system.primaryUser = "user";
-  system.stateVersion = 5;
+ nixpkgs.hostPlatform = "aarch64-darwin";
+ nixpkgs.config.allowUnfree = true;
+ system.primaryUser = "user";
+ system.stateVersion = 5;
 
-  # fish
-  programs.fish.enable = true;
+ # fish
+   programs.fish.enable = true;
+   environment.shells = [ pkgs.fish ];
+   users.knownUsers = [ "user" ];
+   users.users.user = {
+     uid = 501;
+     shell = pkgs.fish;
+   };
 }
