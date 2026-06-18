5907ab77be1ad036e1c495df3cb8ec9a64dac996
paul <198573042+fyrrmi@users.noreply.github.com>
2026-06-18 17:25:50 +0200

    refactor(desktop): remove plasma6 fallback session

--- modules/desktop/sddm.nix
+++ modules/desktop/sddm.nix
@@ -6,7 +6,6 @@
   services.xserver.enable = true;
 
   services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
   services.displayManager.defaultSession = "hyprland";
 
   services.xserver.xkb = {
