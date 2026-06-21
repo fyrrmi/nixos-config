f1e5b58d9330b519c8a69c153cfd261f5a27d931
paul <198573042+fyrrmi@users.noreply.github.com>
2026-06-21 13:57:46 +0200

    fix(gaming): close unterminated fetchFromGitHub block

--- modules/programs/gaming.nix
+++ modules/programs/gaming.nix
@@ -1,5 +1,5 @@
 # module: programs/gaming
-# steam + proton-ge + compatibilité manettes nintendo
+# steam + proton-ge + nintendo controller support
 { pkgs, ... }:
 
 {
@@ -10,18 +10,19 @@
 
   environment.systemPackages = [
     pkgs.archipelago
+    pkgs.poptracker # progression tracker for archipelago randomizers
     (pkgs.sm64coopdx.overrideAttrs (old: {
-              version = "1.5.1";
-              src = pkgs.fetchFromGitHub {
-                owner = "coop-deluxe";
-                repo = "sm64coopdx";
-                tag = "v1.5.1";
-                hash = "sha256:18c3b3y78nyw384wbd1rrgkmd2ssmxr48bgz3jdp81nl71fn79q1";
-              };
-            }))
-       ];
+      version = "1.5.1";
+      src = pkgs.fetchFromGitHub {
+        owner = "coop-deluxe";
+        repo = "sm64coopdx";
+        tag = "v1.5.1";
+        hash = "sha256:18c3b3y78nyw384wbd1rrgkmd2ssmxr48bgz3jdp81nl71fn79q1";
+      };
+    }))
+  ];
 
-  # règles udev manettes nintendo (joy-con, pro controller)
+  # nintendo controller udev rules (joy-con, pro controller)
   services.udev.extraRules = ''
     SUBSYSTEM=="usb", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="0337", MODE="0666"
   '';
